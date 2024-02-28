f7b470f18f9048bbf5467ca98745e5ccd50cb46d
23640056 <23640056@student.uwa.edu.au>
2024-02-28 22:37:06 +0800

    created manifest.json file

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 async function fetchData() {
-    
+    const url = 'https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '70ac4bf157mshd148458076d8deap16ad22jsn849a11e186a3',
+            'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
 }
+fetchData();
